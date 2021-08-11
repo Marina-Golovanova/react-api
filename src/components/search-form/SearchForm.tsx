da6091ea31c1917bedce66608e2babf97eda7ed1
Marina <gma18@list.ru>
2021-08-12 00:41:46 +0300

    feat: completed search-form: add input search and button

--- src/components/search-form/SearchForm.tsx
+++ src/components/search-form/SearchForm.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { InputSearch } from "../input-search/InputSearch";
+import { Button } from "../button/Button";
 
 import styles from "./search-form.module.scss";
 
@@ -7,6 +8,7 @@ export const SearchForm: React.FC = () => {
   return (
     <form className={styles.form}>
       <InputSearch />
+      <Button text="search" onChange={() => console.log} />
     </form>
   );
 };
