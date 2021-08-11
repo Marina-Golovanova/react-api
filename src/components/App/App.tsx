17e21aa95b6ebdd36396e0ee071fafe0b6daccd9
Marina <gma18@list.ru>
2021-08-11 21:07:38 +0300

    feat: add input-search

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Layout } from "../layout/Layout";
+import { SearchForm } from "../search-form/SearchForm";
 
 import "./app.scss";
 
 export const App: React.FC = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <SearchForm />
+    </Layout>
+  );
 };
--- src/components/search-form/SearchForm.tsx
+++ src/components/search-form/SearchForm.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { InputSearch } from "../input-search/InputSearch";
 
 import styles from "./search-form.module.scss";
 
 export const SearchForm: React.FC = () => {
-  return <form className={styles.form}></form>;
+  return (
+    <form className={styles.form}>
+      <InputSearch />
+    </form>
+  );
 };
